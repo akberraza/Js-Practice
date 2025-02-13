07f731f0803955457302bf8ba09409731e559f25
Akbar Raza <akbarraza855@gmail.com>
2025-02-12 23:40:55 -0800

    To day practice

---
+++ app.js
@@ -0,0 +1,828 @@
+// var num ="akbar"
+// var num2 ="raza"
+
+// var addition = num+num2
+
+// console.log(addition)
+
+
+// var num =40
+// var num1 =20
+// var num3 =300
+
+// var addition = num+num1
+// var subtration = num-num1
+// var divide = num/num1
+// var modules = num%num1
+
+// console.log (addition)
+// console.log (subtration)
+// console.log (divide)
+// console.log (modules)
+
+
+
+// var modules = 545 % 2
+
+// console.log(modules)
+
+
+// var num =40
+// var num1 =20
+// var num3 =300
+
+// var bold = num/num1+num3
+// console.log(bold)
+
+
+// var myNum = 10
+
+// var user_num =prompt ("Guess your num");
+
+// if(myNum==user_num){
+//     document.write(`I guess ${myNum} and you guess ${user_num} your are win`)
+// }
+
+// else if (user_num== 11){
+//     document.write(`I guess ${myNum} and you guess ${user_num} your close enough`)
+
+// }
+// else if (user_num == 9 ){
+//     document.write(`I guess ${myNum} and you guess ${user_num} your close enough`)
+
+// }
+// else{
+//     document.write(`I guess ${myNum} and you guess ${user_num} Try again`)
+// }
+
+// var gander = prompt("Enter your Gander")
+// var message= "not a match"
+
+// if(gander === "male"){
+//     message= "Hello sir"
+// }
+// if(gander === "female"){
+//     message = "Hello mam"
+// }
+// alert(message)
+
+
+// comparsion operaters
+// var age = 20
+
+// if(age >= 20){
+//     document.write("OK")
+// }
+
+// var soda = prompt("kya lana ha")
+
+// if (soda == "gosht"){
+//     document.write("to lay ana")
+// }
+
+// else if (soda == "bangan"){
+//     document.write("nh lana")
+// }
+// else if(soda == "phali"){
+//     document.write("lazmi lana")
+// }
+// else{
+//     document.write("kuch bhi lay ayao")
+// }
+
+// var language = prompt("Enter language")
+
+// if(language == "javascript"){
+//     alert("your are hired")
+// }
+// else{
+//     alert("javascript sikh k ayao")
+// }
+
+
+// var userName = prompt("Enter your name")
+// var math = +prompt("Enter maths marks")
+// var english = +prompt("Enter English marks")
+// var obtainedMarks = math + english 
+// var perc =  (obtainedMarks / 200) * 100
+// var grade;
+
+// if(perc >= 80){
+//     grade="A-one"
+// }
+// else if(perc >= 70){
+//     grade ="A"
+// }
+// else{
+//     grade = "Pass"
+// }
+
+// document.write(
+//     `
+//     <table border="2px">
+//     <tr>
+//     <th>NAME </th>
+//     <th>Maths</th>
+//     <th>English</th>
+//     <th>Obtained Marks</th>
+//     <th>Percatage</th>
+//     <th>Grade</th>
+//     </tr>
+
+//     <tr>
+//     <td>${userName}</td>
+//     <td>${math}</td>
+//     <td>${english}</td>
+//     <td>${obtainedMarks}/200</td>
+//     <td>${perc}</td>
+//     <td>${grade}</td>
+//     </tr>
+
+//     </table>
+//     `
+// )
+
+
+// var math = +prompt("Enter math marks")
+// var english = +prompt("Enter English marks ")
+// var urdu = +prompt("Enter Urdu marks")
+// var obtainedMarks = math + english + urdu
+// var perc = (obtainedMarks / 300) * 100;
+// var grade;
+// var re;
+
+// var math = +prompt("Enter math marks")
+// var english = +prompt("Enter English marks ")
+// var urdu = +prompt("Enter Urdu marks")
+// var obtainedMarks2 = math + english + urdu
+// var perc2 = (obtainedMarks2 / 300) * 100;
+// var grade2;
+// var re2;
+
+// var math = +prompt("Enter math marks")
+// var english = +prompt("Enter English marks ")
+// var urdu = +prompt("Enter Urdu marks")
+// var obtainedMarks = math + english + urdu
+// var perc = (obtainedMarks / 300) * 100;
+// var grade;
+// var re;
+
+// if(perc2 >= 80){
+//     grade2 = "A-one"
+//     re2 = "Excellent"
+// }
+// if(perc2 >= 70){
+//     grade2 = "A"
+//     re2 ="Good"
+// }
+// if(perc2 >= 60){
+//     grade2 = "B"
+//     re2 = "You need to improve"
+// }
+// else{
+//     grade2 = "Fail"
+//     re2 = "Sorry"
+// }
+
+
+
+
+
+// document.write(
+//     `
+//     <table border="2px">
+
+//     <tr>
+//     <td>Percentage %</td>
+//     <td>Grade</td>
+//     <td>Remarks</td>
+//     </tr>
+
+//     <tr>
+//     <td>${perc}<td>
+//     <td>${grade}<td>
+//     <td>${re}<td>
+//     </tr>
+
+//     <tr>
+//     <td>${perc}<td>
+//     <td>${grade}<td>
+//     <td>${re}<td>
+//     </tr>
+
+//     <tr>
+//     <td>${perc}<td>
+//     <td>${grade}<td>
+//     <td>${re}<td>
+//     </tr>
+//     </table>
+//     `
+// )
+
+// var days = 3;
+
+// switch (days) {
+
+//     case 1:
+//         document.write("Monday")
+//         break;
+//     case 2:
+//         document.write("Tesday")
+//         break;
+//     case 3:
+//         document.write("Wednessday")
+//         break;
+//     case 4:
+//         document.write("Tusday")
+//         break;
+//     case 5:
+//         document.write("Friday")
+//         break;
+//     case 6:
+//         document.write("Saturday")
+//         break;
+//     case 7:
+//         document.write("Sunday")
+//         break;
+//     default:
+//         document.write("Enter Valid Day")
+// }
+
+// var students=[`Akbar`, `Raza`, `Asjad`];
+//  students[3]= "ALi";
+
+// console.log(students.length);
+
+// var user = prompt("enter table number")
+// var user2 = prompt("enter end")
+
+// for(i=1; i<=user2; i++){
+// document.write(`${user}x ${i} = ${user* i} <br>`)
+// }
+
+// var table1 = prompt("Enter your num table")
+// var table2 = prompt("Enter 2nd num")
+
+// for(tab=1; tab<=table2; tab++){
+//    document.write(`${table1} x ${tab} = ${table1*tab} <br>`)
+// }
+
+
+
+// for(var i=1; i<=5; i++ ){
+
+// for(var j=1; j<=i; j++){
+//     document.write(j + " ")
+// }
+//   document.write("<br>")
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+// for(var i=1; i<=5; i++){
+
+//    for(var j=1; j<=5; j++){
+//     document.write("5" + " ")
+//    }
+//   document.write("<br>")
+
+// }
+
+
+
+
+
+// var cities = ["Karachi" , "Islamabad"];
+// var user = prompt("Enter city Name");
+// var match = false
+
+// for(var i = 0; i < cities.length; i++){
+//   if(user == cities[i]){
+//     document.write(`${user} is the cleanest city `)
+//      match = true
+//      break;
+//   }
+// }
+
+// if(match == false){
+//   document.write(`${user} is the dirty city`)
+// }
+
+
+
+
+
+// var cites = ["karachi" , "islamabad"];
+// var user = prompt("Enter Cite Name").toLowerCase();
+// var match = false
+
+// for(var i = 0; i < cites.length; i++){
+//     if(user == cites[i]){
+//         document.write(`${user} is the cleanist city`)
+//         match = true;
+//         break
+//     }
+// }
+
+// if(match == false){
+//     document.write(`${user } is the dirty city`)
+// }
+
+
+// var cars = ["city","mehran","audi","foxi"];
+// var carsName = prompt("Enter car Name").toLowerCase();
+
+// for(var i=0; i<)
+
+
+// var nums = [2,4,6,5,1,8,3];
+// var min = nums[0];
+
+// for(var i=0; i<nums.length; i++){
+//     if(nums[i]<min){
+//         min = nums[i];
+//     }
+// }
+// document.write(min);
+
+
+// var arr = [2,4,6,5,1,8,3];
+// var min = arr[0];
+
+// for(var i=0; i<arr.length; i++){
+//     if(arr[i]<min){
+//         min = arr[i]
+//     }
+// }
+// document.write(min)
+
+
+// for(var i=0; i<5; i++){
+//     for(var j =0; j<5; j++){
+//         console.log(i,j);
+//     }
+// }
+
+// for(var i =0; i<6; i++){
+//     for(var j =0; j<5; j++){
+//         console.log(i,j)
+//     }
+// }
+
+
+// var arr = [
+//     ["Akbar","Raza","smit"],
+//     ["Hamza","Imtiaz"]
+// ];
+
+// for(var i=0; i<arr.length; i++){
+//     for(var j =0; j<arr[i].length; j++){
+//         document.write(arr[i][j] + "<br>")
+//     }
+// } 
+
+// var arr = [
+//     ["Akbar","Raza"],
+//     ["Hamza","Imtiaz"]
+// ]
+
+// for(var i=0; i<arr.length; i++){
+//     for(var j =0; j<arr[i].length; j++){
+//         document.write(arr[i][j]+"<br>")
+//     }
+// }
+
+
+// var num = "AKBAR RAZA";
+// document.write(num.toLowerCase() + "<br>");
+
+// var name = "akbar raza";
+// document.write(name.toUpperCase());
+
+// var fullName = "akbar";
+// var firstchar = fullName.slice(0,1).toUpperCase();
+// var result = firstchar + fullName.slice(1)
+
+// console.log(result)
+
+
+// var fullName = "Akbar";
+// var firstchar = fullName.slice(0,1).toLowerCase();
+// var result =  firstchar + fullName.slice(1);
+// console.log(result);
+
+
+// var  myName = "My Name is Akbar Raza";
+
+// for(var i=0; i<myName.length; i++){
+//     console.log(myName.slice(i,i+5)==="Akbar")
+// }
+
+// var myName = "My name is Akbar RAZA";
+
+// for(var i = 0; i<myName.length; i++){
+//  console.log(myName.slice(i,i+4)==="RAZA")
+// }
+
+// var text = "Angelina Jolie reveals daughter Shiloh Jolie-Pitt‘s take on celebrity life";
+// var user = prompt("Enter any word");
+
+// for(var i=0; i<text.length; i++){
+//     if(text.slice(i,i+user.length)===user){
+//         console.log("found")
+//     }
+// }
+
+// var text = "Angelina Jolie reveals daughter Shiloh Jolie-Pitt‘s take on celebrity life";
+// var user = prompt("Enter any Word");
+
+// for(var i =0; i<text.length; i++){
+//     if(text.slice(i,i+user.length)===user){
+//           console.log(user)
+//     }
+
+// }
+
+
+// var text = "Angelina Jolie reveals daughter Shiloh Jolie-Pitt‘s take on celebrity life";
+// var user = prompt("Enter any word");
+
+// for(var i =0; i <text.length; i++){
+//      if(text.slice(i,i+user.length)===user){
+//         console.log("Found")
+//      }
+
+//     }
+
+// var text = "Angelina Jolie reveals daughter Shiloh Jolie-Pitt‘s take on celebrity life";
+// console.log(text.indexOf("reveals"));
+
+
+// var text = "Angelina Jolie reveals daughter Shiloh Jolie-Pitt‘s take on celebrity life";
+
+// if(text.indexOf("AKBAR") !== -1){
+//     console.log('Found')
+// }
+// else{
+//     console.log('Not Found')
+// }
+
+// var text = "Angelina Jolie reveals daughter Shiloh Jolie-Pitt‘s take on celebrity life";
+
+// if(text.indexOf("Angelina")!== -1){
+//     console.log("Found")
+// }
+// else{
+//     console.log("Not Found")
+// }
+
+// var num= "Akbar Raza";
+// console.log(num.charAt(0));
+
+// var num = "Akbar Raza";
+// console.log(num[0]);
+
+// var num = 13333;
+// console.log(typeof(num))
+
+// var num = "Akbar Raza";
+// console.log(typeof(num))
+
+// var num = true;
+// console.log(typeof(num))
+
+// var eq = 10 + "10" * 2 + -4 + "haha";
+// // document.write(eq)
+
+// var a = 3
+// var b = 3
+// var eq = --a + b-- + --b - b-- + a++ + b++ - --a + ++a + a + b + 2 
+//         // 2 +  3  +  1  -  3   +  3 +  2  -   3 +  4 +  4 + 3 +2
+//         //    5      +      2      +    5     -     7     +  7  + 2
+//         //       7                 +          2          +      9
+//         //                        18 
+
+//         console.log(eq)
+
+
+
+// var str = "Akbar Raza";
+// console.log(str.replace("Raza","Asjadi"))
+
+// var str = "Akbar raza";
+// console.log(str.replace("Akbar","Asjadi"));
+
+
+
+// var str = "Akbar raza";
+// console.log(str.replaceAll("Asjad"));
+
+
+// var str = "IMF mission will visit to assess governance, corruption risks: mission finance ministry";
+// console.log(str.replaceAll("mission","pakistan"))
+
+// var num = 10.9
+// console.log(Math.round(num))
+
+// var num = 1.6
+// console.log(Math.round(num))
+
+// var num = 20.9
+// console.log(Math.ceil(num))
+
+// var num = Math.round(Math.random()  * 10);  
+// console.log(num)
+
+
+// var heads = prompt("Enter player 1 ");
+// var tails = prompt("Enter player 2");
+
+// var toss = Math.floor(Math.random()*2);
+// console.log(toss)
+// if(toss == 0 ){
+//     alert(heads + "Wins the Toss");
+// }
+// else{
+//     alert(tails + "Wins the Toss")
+// }
+
+// var heads = prompt("Enter player 1");
+// var tails = prompt("Enter player 2");
+// var toss =  Math.random()*2;
+// console.log(toss)
+
+
+// var num = 1233344
+// console.log(num.toString())
+
+
+
+// var cars = ['Audi','Bmw','Mehran','Alto','Foxi','Mehran']
+
+
+// var userInp = prompt('Enter any car')
+// var firstAlpha = userInp.slice(0,1).toUpperCase()
+// var remainAlpha = userInp.slice(1).toLowerCase()
+// var final = firstAlpha + remainAlpha
+// var match = false
+
+// for(var i=0; i < cars.length; i++){
+//     if(final == cars[i]){
+//         match = true
+//         document.write(final + " car found")
+//         break
+//     }
+// }
+// if(match == false){
+//     document.write(final + ' car not found')
+// }
+
+
+// var cars = ['audi','bmw','mehran','alto','foxi','mehran']
+
+
+
+// for(var i=0; i < cars.length; i++){
+//     var a = cars[i][0].toUpperCase()
+//     var b = cars[i].slice(1).toLowerCase()
+//     var final = a + b 
+//     document.write( final + "<br>")
+// }
+
+
+// var cars = ["audi","bmw","mehran","alto"];
+
+// for(var i = 0; i <cars.length; i++){
+//     var a = cars[i][0].toUpperCase();
+//     var b = cars [i].slice(1).toLowerCase();
+//     var final = a + b;
+//     console.log(final)
+// }
+
+
+// var cars = ["audi","bmw","mehran","alto"];
+
+// for(var i = 0; i<cars.length; i++){
+//     var a = cars[i][0].slice(0,1).toLowerCase();
+//     var b = cars[i][1].toUpperCase();
+//     var c = cars[i].slice(2).toLowerCase();
+//    var final= a + b + c
+//    console.log(final)
+// }
+
+
+// Question # 1
+
+// var firstName = prompt("Enter First Name");
+// var lastName = prompt("Enter Last Name");
+// var fullName = firstName + lastName;
+// console.log(fullName)
+
+// Question # 2
+
+// var mobile = prompt("Enter your favorite mobile and Modile");
+// console.log(mobile.length)
+
+
+// Question # 3
+
+// var str = "pakistani";
+// console.log(str.indexOf("n"));
+
+// Question # 4
+
+// var str = "Hello World";
+// console.log(str.lastIndexOf("l"))
+
+// Question # 5
+
+// var str = "Pakistani";
+// console.log(str.charAt(3));
+
+// Question # 6
+
+// var firstName = prompt("Enter First Name");
+// var lastName = prompt("Enter Last Name");
+// var fullName = firstName.concat(lastName);
+// console.log(fullName)
+
+// Question # 7
+
+// var nam = "Hyderabad";
+// console.log(nam.replace("Hyder","Islam"));
+
+// Question # 8
+
+// var message = "Ali and sami best friends.They play circket and football togather;";
+// console.log(message.replaceAll("and","&"));
+
+// Question # 9
+
+// var num = "472";
+// console.log("Value "+num);
+// console.log(typeof("Type " + num));
+// var num1 = parseInt("472");
+// console.log(+num1)
+// console.log(typeof( num1))
+
+// Question # 10
+
+// var user = prompt("Enter ");
+// document.write(user.replace("www.",""))
+
+
+// Question # 11
+
+// var peanuts = "peanuts".toUpperCase();
+// console.log(peanuts)
+
+// Question # 12
+
+// var user = prompt("Enter ZONG 4G").toLowerCase();
+// document.write(user)
+
+// Question # 13
+
+// var str = "javaScript";
+// var convert = str.slice(0,1).toUpperCase();
+// var convert1 = str.slice(1).toLowerCase();
+// var final = convert + convert1
+// console.log(final)
+
+
+// Question # 14
+
+// var num = "35.36";
+// console.log(num.toString().replace(".",""))
+
+
+// Question # 15
+
+// var a = "3";
+// var b = "3" ;
+// var x = a + b;
+// document.write(`a is ${a} <br> b is ${b} <br> a + b is  ${a+b}`)
+
+// Question # 16
+
+// var a = "3";
+// var b = "3"
+// var y = a - b;
+// document.write(`a is ${a} <br> b is ${b} <br> a - b is ${a - b}`)
+
+// Question # 17
+
+// var speical = ["!","@","&",",","."];
+// var user = prompt("Enter a Valid Name");
+// var valid = true;
+
+// for(var i=0; i<user.length; i++){
+//     if(speical.includes(user[i])){
+//         valid = false;
+//         break;
+//     }
+// }
+
+// if(!valid){
+//     alert("Invalid Name")
+// }
+// else{
+//     alert("Your Name is Valid")
+// }
+
+
+
+// Question # 18
+
+// var beakry = ["cake","apple pie","cookie","chips","patties"];
+// var user = prompt("Enter any itme").toLowerCase();
+
+// if(beakry.includes(user)){
+//     alert(`${user} is available at index of ${beakry.indexOf(user)} in our beakry`)
+// }
+// else{
+//     alert(`We are sorry ${user} is not avaliable in our bakery`)
+// }
+
+
+
+
+
+// Question # 19
+
+// var str1 = prompt("Enter any thing")
+// var str2 = prompt("Enter any thing")
+
+// if(str1 > str2){
+//     document.write(`${str1} is greater then ${str2}`)
+// }
+// else if(str1 < str2){
+//     document.write(`${str1} is less ${str2}`)
+// }
+// else{
+//     document.write("Both are equal")
+// }
+
+// ceil method
+
+
+// var user = Number(prompt("Enter any Number with decimal"));
+// var num = Math.ceil(user);
+// document.write(num);
+
+// floor method\
+
+// var user = Number(prompt("Enter any Number in Decimal"));
+// var nums = Math.floor(user);
+// document.write(nums)
+
+// var heasds = prompt("Player 1");
+// var tails = prompt("Player 2");
+// var toss =     Math.floor(Math.random() *2);
+// if(toss == 0){
+//     alert(`${heasds} is toss win`)
+// }
+// else{
+//    alert(`${tails} is toss win`)
+// }
+
+// var nums =  Math.floor(Math.random(1) * 100);
+// console.log(nums)
+
+var nums = Math.floor(Math.random(1) *10);
+var user = +prompt("Enter any number 1 to 10");
+if(nums == user){
+    alert("You Win")
+}
+else{
+    alert("Try Again")
+}
+
+// Loop practice
+
+
+// for(var i =1; i <6; i++){
+//     document.write(i + "Hello World" + "<br>")
+// }
+
+// for(var i =1; i<11; i++){
+//     document.write(i + "<br>")
+// }
+
+
+// var table = +prompt("Enter table Number");
+// var length = +prompt("Enter table Length");
+
+// for(var i =1; i <= length; i++){
+//     document.write(`${table} x ${i} = ${table * i} <br>`)
+// }
