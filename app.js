4c4d6198a433d0f4e90085421d454efc67b1600a
Akbar Raza <akbarraza855@gmail.com>
2025-05-04 21:55:28 +0500

    last practice

--- app.js
+++ app.js
@@ -3666,3 +3666,492 @@ function logout(){
   localStorage.clear();
   location.href = "./index.html"
 }*/
+
+/*  Quiz Application  */
+
+/*var questions = [
+  {
+    question: "What does HTML stand for?",
+    option1: "Hyperlinks and Text Markup Language",
+    option2: "Hypertext Markup Language",
+    option3: "Home Tool Markup Language",
+    correctOption: "Hypertext Markup Language",
+  },
+  {
+    question: "Who is making the Web standards?",
+    option1: "Google",
+    option2: "The World Wide Web Consortium",
+    option3: "Microsoft",
+    correctOption: "The World Wide Web Consortium",
+  },
+  {
+    question: "What is the purpose of CSS?",
+    option1: "To structure a webpage",
+    option2: "To style a webpage",
+    option3: "To handle databases",
+    correctOption: "To style a webpage",
+  },
+  {
+    question: "What is Bootstrap?",
+    option1: "A programming language",
+    option2: "A design framework",
+    option3: "A type of database",
+    correctOption: "A design framework",
+  },
+  {
+    question: "Which tag is used to insert an image in HTML?",
+    option1: "<img>",
+    option2: "<image>",
+    option3: "<pic>",
+    correctOption: "<img>",
+  },
+  {
+    question: "Choose the correct HTML element for the largest heading:",
+    option1: "<heading>",
+    option2: "<h6>",
+    option3: "<h1>",
+    correctOption: "<h1>",
+  },
+  {
+    question: "What is the correct HTML element for inserting a line break?",
+    option1: "<linebreak>",
+    option2: "<br>",
+    option3: "<break>",
+    correctOption: "<br>",
+  },
+  {
+    question: "What is the correct HTML for adding a background color?",
+    option1: '<body bg="yellow">',
+    option2: "<background>yellow</background>",
+    option3: '<body style="background-color:yellow;">',
+    correctOption: '<body style="background-color:yellow;">',
+  },
+  {
+    question: "Which CSS property is used to change text color?",
+    option1: 'font',
+    option2: "color",
+    option3: 'text',
+    correctOption: 'color',
+  },
+  {
+    question: "Which attribute is used in HTML to provide alternative text for an image?",
+    option1: 'title',
+    option2: "alt",
+    option3: 'src',
+    correctOption: 'alt',
+  }
+]; 
+
+
+
+var getQues = document.getElementById("qeus");
+var getOption1 = document.getElementById("opt1");
+var getOption2 = document.getElementById("opt2");
+var getOption3 = document.getElementById("opt3");
+var getInputs = document.getElementsByTagName("input");
+var getBtn =  document.getElementById("btn");
+var getQuestionNumber = document.getElementById("question-number");
+var getTimer = document.getElementById("timer");
+
+var index = 0;
+var score = 0; 
+var trimeInterval;
+var autoNextTimeout;
+
+function startTimer(){
+  var timeLeft = 10;
+  getTimer.innerHTML = "Time left: " + timeLeft + "s";
+
+  trimeInterval = setInterval(function () {
+     timeLeft--;
+     getTimer.innerHTML = "Time left: " + timeLeft + "s";
+     if(timeLeft <= 0){
+       clearInterval(trimeInterval);
+     }
+  },1000)
+};
+
+function loadQuestion(){
+   clearInterval(trimeInterval);
+   clearTimeout(autoNextTimeout);
+
+   if(index >= questions.length){
+    showResult();
+    return;
+   }
+
+for(var i = 0; i < getInputs.length; i++){
+     getInputs[i].checked = false;
+     getInputs[i].parentElement.classList.remove("active");
+     getInputs[i].parentElement.querySelector("i").className = "far fa-circle";
+}
+
+var q = questions[index];
+getQues.innerText = q.question;
+getOption1.innerText = q.option1;
+getOption2.innerText = q.option2;
+getOption3.innerText = q.option3;
+
+getQuestionNumber.innerText = `Question ${index + 1} / ${questions.length}`;
+getBtn.disabled = true;
+
+startTimer();
+
+autoNextTimeout = setTimeout(function () {
+  checkAnswer();
+  index++;
+  loadQuestion();
+},10000);
+}
+
+function enableNext(){
+  getBtn.disabled = false;
+  for(var i = 0; i < getInputs.length; i++){
+   getInputs[i].parentElement.classList.remove("actic");
+   getInputs[i].parentElement.querySelector("i").className = "far facircle";
+
+   if(getInputs[i].checked){
+    getInputs[i].parentElement.classList.add("active");
+    getInputs[i].parentElement.querySelector("i").className = "far fa-check-circle";
+   }
+  }
+}
+
+function next(){
+  clearInterval(trimeInterval);
+  clearTimeout(autoNextTimeout);
+  checkAnswer();
+  index++;
+  loadQuestion();
+}
+
+function checkAnswer(){
+  for(var i = 0; i < getInputs.length; i++){
+    if(getInputs[i].checked){
+      var selectedText = document.querySelector("#opt"+(i+1)).innerHTML;
+      if(selectedText == questions[index].correctOption){
+         score++;
+      }
+      break;
+    }
+  }
+}
+
+function showResult(){
+  clearInterval(trimeInterval);
+  clearTimeout(autoNextTimeout);
+
+  var percentage = (score / questions.length) * 100;
+  var status = percentage >= 75 ? "Pass" : "Fail";
+
+  Swal.fire({
+    title: `Quiz Ended!`,
+    html: `Your Score: <strong>${score}/${questions.length}</strong> <br> Percentage: <strong>${percentage}%</strong>`,
+    icon: status === "Pass" ? "success" : "error",
+    showCancelButton : true,
+    confirmButtonText: "Restart",
+    cancelButtonText: "Cancel",
+  }).then((result) =>{
+    if(result.isConfirmed){
+     index = 0;
+     score = 0;
+
+     setTimeout(() =>{
+      loadQuestion();
+     },300);
+    }else{
+      Swal.fire("Quiz Cancelled","","info");
+    }
+  })
+}
+
+loadQuestion();*/
+
+
+/*var allUsers = JSON.parse(localStorage.getItem("user")) || [];
+var getUl = document.querySelector("#ul")
+
+function randerUser(){
+   getUl.innerHTML = "";
+   for(var i = 0; i < allUsers.length; i++){
+      getUl.innerHTML += `<li>${allUsers[i].name} " ${allUsers[i].email} " ${allUsers[i].password}</li>`
+   }
+}
+randerUser();
+
+function btn(){
+   var getName = document.querySelector("#name");
+   var getEmail = document.querySelector("#email");
+   var getPassword = document.querySelector("#password");
+
+   var obj = {
+      name: getName.value,
+      email: getEmail.value,
+      password: getPassword.value
+   }
+
+   allUsers.push(obj);
+   localStorage.setItem("user",JSON.stringify(allUsers));
+
+   getName.value = "";
+   getEmail.value = "";
+   getPassword.value = "";
+
+   randerUser();
+}
+*/
+
+
+/*var questions = [
+  {
+    question: "What does HTML stand for?",
+    option1: "Hyperlinks and Text Markup Language",
+    option2: "Hypertext Markup Language",
+    option3: "Home Tool Markup Language",
+    correctOption: "Hypertext Markup Language",
+  },
+  {
+    question: "Who is making the Web standards?",
+    option1: "Google",
+    option2: "The World Wide Web Consortium",
+    option3: "Microsoft",
+    correctOption: "The World Wide Web Consortium",
+  },
+  {
+    question: "What is the purpose of CSS?",
+    option1: "To structure a webpage",
+    option2: "To style a webpage",
+    option3: "To handle databases",
+    correctOption: "To style a webpage",
+  },
+  {
+    question: "What is Bootstrap?",
+    option1: "A programming language",
+    option2: "A design framework",
+    option3: "A type of database",
+    correctOption: "A design framework",
+  },
+  {
+    question: "Which tag is used to insert an image in HTML?",
+    option1: "<img>",
+    option2: "<image>",
+    option3: "<pic>",
+    correctOption: "<img>",
+  },
+  {
+    question: "Choose the correct HTML element for the largest heading:",
+    option1: "<heading>",
+    option2: "<h6>",
+    option3: "<h1>",
+    correctOption: "<h1>",
+  },
+  {
+    question: "What is the correct HTML element for inserting a line break?",
+    option1: "<linebreak>",
+    option2: "<br>",
+    option3: "<break>",
+    correctOption: "<br>",
+  },
+  {
+    question: "What is the correct HTML for adding a background color?",
+    option1: '<body bg="yellow">',
+    option2: "<background>yellow</background>",
+    option3: '<body style="background-color:yellow;">',
+    correctOption: '<body style="background-color:yellow;">',
+  },
+  {
+    question: "Which CSS property is used to change text color?",
+    option1: 'font',
+    option2: "color",
+    option3: 'text',
+    correctOption: 'color',
+  },
+  {
+    question: "Which attribute is used in HTML to provide alternative text for an image?",
+    option1: 'title',
+    option2: "alt",
+    option3: 'src',
+    correctOption: 'alt',
+  }
+]; 
+
+var getQuestionNumber = document.querySelector("#question-number");
+var getQues = document.querySelector("#ques");
+var getInputs = document.getElementsByTagName("input");
+var getOption1 = document.querySelector("#opt1")
+var getOption2 = document.querySelector("#opt2")
+var getOption3 = document.querySelector("#opt3")
+var getTimer  = document.querySelector("#timer");
+var getBtn = document.querySelector("#btn");
+
+var index = 0;
+var score = 0;
+var timeInterval;
+var autoNextTimeout;
+
+function startTimer(){
+  var timeLeft = 10;
+  getTimer.innerHTML = "Time Left: " + timeLeft + "s";
+  
+  timeInterval = setInterval(function(){
+        timeLeft--;
+        getTimer.innerHTML = "Time left: " + timeLeft + "s";
+        if(timeLeft <= 0){
+          clearInterval(timeInterval);
+    }
+  },1000)
+};
+
+function loadQuestion(){
+  clearInterval(timeInterval);
+  clearTimeout(autoNextTimeout);
+
+  if(index >= questions.length){
+    showResult();
+    return;
+  }
+
+   for(var i = 0; i < getInputs.length; i++){
+    getInputs[i].checked = false;
+    getInputs[i].parentElement.classList.remove("active");
+    getInputs[i].parentElement.querySelector("i").className = "far fa-circle";
+   }
+
+   var q = questions[index];
+   getQues.innerText = q.question;
+   getOption1.innerText = q.option1;
+   getOption2.innerText = q.option2;
+   getOption3.innerText = q.option3;
+   
+
+}*/
+
+/*var arr = [];
+
+var user = localStorage.getItem("user");
+
+if(user !== null){
+  arr = JSON.parse(user);
+}
+
+function signup(){
+  var getName = document.querySelector("#name");
+  var getEmail = document.querySelector("#email");
+  var getPassword = document.querySelector("#password");
+
+  var obj = {
+    name: getName.value,
+    email: getEmail.value,
+    password: getPassword.value
+  }
+
+  arr.push(obj);
+  localStorage.setItem("user",JSON.stringify(arr));
+
+  getName.value = "";
+  getEmail.value = "";
+  getPassword.value = "";
+}
+
+
+function login(){
+  var getEmail = document.querySelector("#email");
+  var getPassword = document.querySelector("#password");
+
+  var filterUser = arr.filter(function(data){
+    return data.email == getEmail.value && data.password == getPassword.value;
+  })
+
+  if(filterUser.length){
+    alert("User login successfully!")
+  }
+  else{
+    alert("Email or Password Incorrect!")
+  }
+}*/
+
+/*var arr = [];
+
+var user = localStorage.getItem("user");
+
+if(user !== null){
+  arr = JSON.parse(user);
+}
+
+function signup(){
+  var getName = document.querySelector("#name");
+  var getEmail = document.querySelector("#email");
+  var getPassword = document.querySelector("#password");
+
+  var obj = {
+    name: getName.value,
+    email: getEmail.value,
+    password: getPassword.value
+  }
+
+  arr.push(obj);
+  localStorage.setItem("user",JSON.stringify(arr));
+
+  getName.value = "";
+  getEmail.value = "";
+  getPassword.value = "";
+}
+
+function login(){
+  var getEmail = document.querySelector("#email");
+  var getPassword = document.querySelector("#Password");
+
+  var filterUser = arr.filter(function(data){
+    return data.email == getEmail.value &&  data.password == getPassword.value;
+  })
+
+  if(filterUser.length){
+    alert("User login Successfully!")
+  }
+  else{
+    alert("Email or Password inCorrect!")
+  }
+}*/
+
+/*var arr = [];
+
+var user = localStorage.getItem("user");
+
+if(user !== null){
+   arr = JSON.parse(user);
+}
+
+function signup() {
+  var getName = document.querySelector("#name");
+  var getEmail = document.querySelector("#email");
+  var getPassword = document.querySelector("#password");
+
+  var obj = {
+    name: getName.value,
+    email: getEmail.value,
+    password: getPassword.value
+  }
+
+  arr.push(obj);
+  localStorage.setItem("user", JSON.stringify(arr));
+
+  getName.value = "";
+  getEmail.value = "";
+  getPassword.value = "";
+}
+
+function login() {
+  var getEmail = document.querySelector("#email");
+  var getPassword = document.querySelector("#password");
+
+  var filterUser = arr.filter(function(data){
+    return data.email == getEmail.value && data.password == getPassword.value
+  })
+
+  if(filterUser.length){
+   alert("uesr login Successfully!");
+  }
+  else{
+    alert("Email or Password inCorrect!");
+  }
+}*/
